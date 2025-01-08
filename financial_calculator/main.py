3de92125f088dc64950a1d40e8668217bbdafd6f
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-01-08 10:40:29 -0700

    first commit

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+#Jonas Fairchild, Financial Calculator
+
+#store Balance
+#Add to balance (function)
+#take from balance (function)
+#Calculate time until goal, weekly or monthly (function)
+#Calculate compound interest (function)
+#Allocate budget (function)
+#Calculate sale price (with discounts) (function)
+#Calculate tips (function)
+
+balance = 0
