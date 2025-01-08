360b98d743d62a26e7ed95dcd0d0b644cf2bc7ca
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-01-08 10:50:40 -0700

    Financial Calculator main function

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,12 +1,20 @@
 #Jonas Fairchild, Financial Calculator
 
 #store Balance
-#Add to balance (function)
-#take from balance (function)
+#Add to balance
+#take from balance
 #Calculate time until goal, weekly or monthly (function)
 #Calculate compound interest (function)
 #Allocate budget (function)
 #Calculate sale price (with discounts) (function)
 #Calculate tips (function)
 
+def main():
+    pass
+
 balance = 0
+def goalTime(): #Calculates the time until a specified goal is reached.
+    goal = int(input("How much money do you want to save?: "))
+    goalType = input("Would you like to create a weekly or monthly payment for your goal?: ").lower()
+    goalAmount = int(input("How much money will you allocate to your goal after this time?: "))
+    print(f"Your goal will take ")
