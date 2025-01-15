d224fe42f65ca654dbe0fbf797c09c4766860668
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-01-15 10:51:12 -0700

    N O T H I N G

---
+++ personal_library/main.py
@@ -0,0 +1,5 @@
+#Stores all items in a list
+#Function to add a new item
+#Function to search the items
+#Function to remove an item
+#Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
