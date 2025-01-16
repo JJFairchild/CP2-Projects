6e6577eeb4a682f65a3490ca3e2cdbbf9b28641e
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-01-16 10:51:19 -0700

    REMOVE IT

--- personal_library/main.py
+++ personal_library/main.py
@@ -1,5 +1,32 @@
-#Stores all items in a list
-#Function to add a new item
+#Stores all items in a set or tuple (done)
+#Function to add a new item (done)
 #Function to search the items
 #Function to remove an item
+#Function to display ALL items
 #Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
+
+songs = (['A', 'A'], ['B', 'B'])
+
+def add():
+    song = input("What song do you want to add to the list?: ")
+    writer = input("Who wrote the song?: ")
+    return tuple(list(songs) + [[song, writer]])
+
+def remove():
+    while True:
+        print("What item do you want to remove from the list?\n\nOptions:")
+        for item in songs:
+            print(item[0], end = "\n")
+        song = input().lower()
+        for item in songs:
+            if item[0].lower() == song:
+                songList = list(songs)
+                del songlist lkzrhglkhsbv
+        else:
+            print("That song isn't on the list.")
+            return songs
+                
+            
+
+
+remove()
