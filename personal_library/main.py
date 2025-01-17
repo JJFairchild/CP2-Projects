3cfdac1c4f562fefbeb042afa13fc97b14fe9abb
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-01-17 14:42:08 -0700

    IT BROkED

--- personal_library/main.py
+++ personal_library/main.py
@@ -5,7 +5,7 @@
 #Function to display ALL items (done)
 #Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
 
-songs = (['A', 'A'], ['B', 'B'])
+songs = ()
 import os
 
 def add():
@@ -14,46 +14,55 @@ def add():
     return tuple(list(songs) + [[song, writer]])
 
 def remove():
-    while True:
-        print("What item do you want to remove from the list?\n\nOptions:")
-        for item in songs:
-            print(item[0], end = "\n")
-        song = input().lower()
-        for item in songs:
-            if item[0].lower() == song:
-                songList = list(songs)
-                del songList[songs.index(item)]
-                return tuple(songList)
-        else:
-            print("That song isn't on the list.")
-            return songs
-                
+    if songs != ():
+        while True:
+            print("What item do you want to remove from the list?\n\nOptions:")
+            for item in songs:
+                print(item[0], end = "\n")
+            song = input().lower()
+            for item in songs:
+                if item[0].lower() == song:
+                    songList = list(songs)
+                    del songList[songs.index(item)]
+                    return tuple(songList)
+            else:
+                print("That song isn't on the list.")
+                return songs
+    else:
+        print("There's nothing to remove.")
+
 def display(list=songs):
-    count = 1
-    for song in list:
-        print(f"{count}. {song[0]}, by {song[1]}.\n")
-        count += 1
+    if songs != ():
+        count = 1
+        for song in list:
+            print(f"{count}. {song[0]}, by {song[1]}.\n")
+            count += 1
+    else:
+        print("The song list is empty.")
 
 def search():
-    searchResults = []
-    if input("Do you want to search by artist or by title?: ").lower()[0] == "a":
-        searchType = 1
+    if songs != ():
+        searchResults = []
+        if input("Do you want to search by artist or by title?: ").lower()[0] == "a":
+            searchType = 1
+        else:
+            searchType = 0
+        search = input("What is your search?: ").lower()
+        for song in songs:
+            if search in song[searchType].lower():
+                searchResults += [song]
+        print("Search results:")
+        if searchResults:
+            display(searchResults)
+        else:
+            print("None")
     else:
-        searchType = 0
-    search = input("What is your search?: ").lower()
-    for song in songs:
-        if search in song[searchType].lower():
-            searchResults += [song]
-    print("Search results:")
-    if searchResults:
-        display(searchResults)
-    else:
-        print("None")
+        print("There's nothing to search for.")
 
 def main():
     while True:
         os.system("cls")
-        choice = input("What do you want to do?\n1. Search the song list\n 2. Add an item to the list\n3. Remove an item from the list\n4. Display the list\n5. Exit the program\n")
+        choice = int(input("What do you want to do?\n1. Search the song list\n2. Add an item to the list\n3. Remove an item from the list\n4. Display the list\n5. Exit the program\n"))
         if choice == 1:
             search()
         elif choice == 2:
@@ -65,3 +74,5 @@ def main():
         elif choice == 5:
             break
         input("Done reading?: ")
+
+main()
