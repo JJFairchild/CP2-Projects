7ea146738143b8a56ebdace34a600063fcfa1f3c
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-01-28 10:19:23 -0700

    Password Generator: bugfixes added

--- password_generator/main.py
+++ password_generator/main.py
@@ -16,7 +16,7 @@ def characters(): #Gets the length of the user's password
     while True:
         try:
             characters = int(input("How many characters should your password have?: "))
-            while characters < 1 or characters > 100:
+            while characters < 1 or characters > 10000:
                 print("That number is too small or too big. Try again.")
                 characters = int(input("How many characters should your password have?: "))
             else:
@@ -25,32 +25,49 @@ def characters(): #Gets the length of the user's password
             print("That isn't a number. Try again.")
 
 def add_lower(): #Asks if lowercase letters should be added and removes them from all_characters if not
-    if input("Should your password include lowercase letters? (Y/n): ").lower()[0] == "n":
+    choice = input("Should your password include lowercase letters? (Y/n): ").lower()
+    if choice == "n":
         del all_characters[3]
         return all_characters
-    else:
+    elif choice == "y":
         return all_characters
+    else:
+        print("Invalid input. Try again.")
+        add_lower()
     
 def add_upper(): #Asks if uppercase letters should be added and removes them from all_characters if not
-    if input("Should your password include uppercase letters? (Y/n): ").lower()[0] == "n":
+    choice = input("Should your password include uppercase letters? (Y/n): ").lower()
+    if choice == "n":
         del all_characters[2]
         return all_characters
-    else:
+    elif choice == "y":
         return all_characters
+    else:
+        print("Invalid input. Try again.")
+        add_upper()
 
 def add_numbers(): #Asks if numbers should be added and removes them from all_characters if not
-    if input("Should your password include numbers? (Y/n): ").lower()[0] == "n":
+    choice = input("Should your password include numbers? (Y/n): ").lower()
+    if choice == "n":
         del all_characters[1]
         return all_characters
-    else:
+    elif choice == "y":
         return all_characters
+    else:
+        print("Invalid input. Try again.")
+        add_numbers()
     
 def add_special(): #Asks if special characters should be added and removes them from all_characters if not
-    if input("Should your password include special characters? (Y/n): ").lower()[0] == "n":
+    choice = input("Should your password include special characters? (Y/n): ").lower()
+    if choice == "n":
         del all_characters[0]
         return all_characters
-    else:
+    elif choice == "y":
         return all_characters
+    else:
+        print("Invalid input. Try again.")
+        add_special()
+    
 
 def password_gen(ch): #Takes the list of all characters and an intended password length, then randomly generates 4 passwords with this information.
     for i in range(1, 5):
@@ -60,13 +77,23 @@ def password_gen(ch): #Takes the list of all characters and an intended password
         print(f"{i}. {password}")
 
 def main(): #Collects all the previous functions neatly, and gets all the information the password_gen function needs to make passwords.
-    ch = characters()
-    all_characters = add_lower()
-    all_characters = add_upper()
-    all_characters = add_numbers()
-    all_characters = add_special()
-    print()
-    password_gen(ch)
-    print()
+    choice = "y"
+    while True:
+        if choice == "y":
+            ch = characters()
+            all_characters = add_lower()
+            all_characters = add_upper()
+            all_characters = add_numbers()
+            all_characters = add_special()
+            print()
+            password_gen(ch)
+            print()
+            choice = input("Print more passwords? (Y/n): ").lower()
+        elif choice == "n":
+            break
+        else:
+            while choice not in ["y", "n"]:
+                print("Invalid input. Try again.")
+                choice = input("Print more passwords? (Y/n): ").lower()
 
 main()
