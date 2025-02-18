6c402389260de66035a80f812b90a54b12177b26
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-02-18 10:50:58 -0700

    Less dysfunctional to-do list

--- to-do_list/main.py
+++ to-do_list/main.py
@@ -3,5 +3,11 @@
 to_do = []
 
 with open("to-do_list/to-do.txt", "r") as file:
-    data = file.read()
-    print(data)
+    data = file.read() + "\n"
+    item = ""
+    for i in data:
+        if i == "\n":
+            to_do.append(item)
+            item = ""
+        else:
+            item += i
