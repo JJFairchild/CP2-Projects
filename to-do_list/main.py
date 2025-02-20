e2ab89bd863733359311a6f58fdc0085cfeb3c58
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-02-20 10:51:05 -0700

    To-do list finished

--- to-do_list/main.py
+++ to-do_list/main.py
@@ -4,7 +4,7 @@ import os
 
 to_do = []
 
-with open("to-do_list/to-do.txt", "r") as file:
+with open("to-do_list/to-do.txt", "r") as file: #Opens the file and adds every line to a list
     data = file.read() + "\n"
     item = ""
     for line in data:
@@ -14,7 +14,11 @@ with open("to-do_list/to-do.txt", "r") as file:
         else:
             item += line
 
-def select_item():
+for i in to_do: #Removes junk from the list
+    if i in ["", "\n"]:
+        to_do.remove(i)
+
+def select_item(): #A function used elsewhere to select a specific item in the to-do list. Returns the index of the item.
     print("What item do you want to select?")
     for line in to_do:
         print(f"- {line}", end="\n")
@@ -24,33 +28,63 @@ def select_item():
             return to_do.index(choice)
         else:
             print("That's not in the list. Try again.")
+            choice = input("What item do you want to select?: ")
 
-def add_item():
+def add_item(): #Adds an item to the to-do list.
     item = input("What item do you want to add?: ")
     to_do.append(item)
     return to_do
 
-def mark_item():
+def mark_item(): 
     if to_do:
         item = select_item()
-        to_do[item]
-
-
+        if " (Completed)" in to_do[item]:
+            print("That item is already completed.")
+        else:    
+            to_do[item] += " (Completed)"
     else:
         print("There are no items to mark complete.")
-        return to_do
+    return to_do
+
+def remove_item():
+    if to_do:
+        item = select_item()
+        del to_do[item]
+    else:
+        print("There are no items to remove.")
+    return to_do
+
+def display_items():
+    if to_do:
+        print("To-do list:")
+        for item in to_do:
+            print(f"- {item}")
+    else:
+        print("There's nothing to display.")
 
 def main():
     while True:
         os.system("cls")
-        match input("What do you want to do?\n1. Add an item\n2. Mark an item as complete\n3. Remove an item\n4. View all items\n5. Exit program"):
+        match input("What do you want to do?\n1. Add an item\n2. Mark an item as complete\n3. Remove an item\n4. View all items\n5. Exit program\n"):
             case "1":
-                pass
+                to_do = add_item()
             case "2":
-                pass
+                to_do = mark_item()
             case "3":
-                pass
+                to_do = remove_item()
             case "4":
-                pass
+                display_items()
             case "5":
                 break
+            case _:
+                print("Invalid input. Try again.")
+        input("Done reading?: ")
+
+main()
+
+with open("to-do_list/to-do.txt", "w") as file: #Deletes the file contents
+    file.write("")
+
+with open("to-do_list/to-do.txt", "a") as file: #Deletes the file contents
+    for i in to_do:
+        file.write(i + "\n")
