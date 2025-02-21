ef76033a2c1c348509d2a84cf860f23e02d244d4
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-02-21 10:26:44 -0700

    To-do list: Added comments and QoL

--- to-do_list/main.py
+++ to-do_list/main.py
@@ -33,28 +33,31 @@ def select_item(): #A function used elsewhere to select a specific item in the t
 def add_item(): #Adds an item to the to-do list.
     item = input("What item do you want to add?: ")
     to_do.append(item)
+    print("Item successfully added.")
     return to_do
 
-def mark_item(): 
+def mark_item(): #Selects a item, then, if it's not already marked as complete, do so.
     if to_do:
         item = select_item()
         if " (Completed)" in to_do[item]:
             print("That item is already completed.")
         else:    
             to_do[item] += " (Completed)"
+            print("Item successfully marked.")
     else:
         print("There are no items to mark complete.")
     return to_do
 
-def remove_item():
+def remove_item(): #Similar to the mark function, but instead of adding a specific string to a specific item, it just removes the item entirely.
     if to_do:
         item = select_item()
         del to_do[item]
+        print("Item successfully removed.")
     else:
         print("There are no items to remove.")
     return to_do
 
-def display_items():
+def display_items(): #Displays every item in the to-do list in a neat order.
     if to_do:
         print("To-do list:")
         for item in to_do:
@@ -62,7 +65,7 @@ def display_items():
     else:
         print("There's nothing to display.")
 
-def main():
+def main(): #Main function that branches out to other parts of the program.
     while True:
         os.system("cls")
         match input("What do you want to do?\n1. Add an item\n2. Mark an item as complete\n3. Remove an item\n4. View all items\n5. Exit program\n"):
@@ -85,6 +88,6 @@ main()
 with open("to-do_list/to-do.txt", "w") as file: #Deletes the file contents
     file.write("")
 
-with open("to-do_list/to-do.txt", "a") as file: #Deletes the file contents
+with open("to-do_list/to-do.txt", "a") as file: #Writes the to-do list to the text file
     for i in to_do:
         file.write(i + "\n")
