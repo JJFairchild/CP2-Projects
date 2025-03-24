c73c4138480e2de22462186da35047d48fe4675e
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-03-24 10:49:39 -0600

    Notes

---
+++ notes/tracing.py
@@ -0,0 +1,59 @@
+#Jonas Fairchild, tracing
+
+"""
+What is tracing?
+    It allows you to see what is happening with your functions. To do it, use: python -m trace --trace [relative path]. You can use:
+    --trace (displays function lines as they are executed)
+    --count (displays the number of times each function is executed)
+    --listfuncs (lists the functions you have)
+    --trackcalls (shows relationship between functions)
+
+What are some ways we can debug by tracing?
+    Make a function that lets us see how our functions are interacting and running
+
+How do you access the debugger in VS Code?
+    F5 or you can run a debug on the left side or you can scroll down under the play button to debug python file
+
+What is testing?
+
+
+What are boundary conditions?
+
+
+How do you handle when users give strange inputs?
+
+
+"""
+
+import trace
+import sys
+
+tracer = trace.Trace(count=False, trace=True)
+def trace_calls(frame, event, arg):
+    if event == 'call':
+        print(f"Calling function: {frame.f_code.co_name}")
+    elif event == 'line':
+        print(f"Executing line: {frame.f_lineno} in {frame.f_code.co_name}")
+    elif event == 'return':
+        print(f"{frame.f_code.co_name} returned {arg}")
+    elif event == 'exception':
+        print(f"Exception in {frame.f_code.co_name}: {arg}")
+    return trace_calls
+
+sys.settrace(trace_calls)
+
+"""
+call - when the function is called
+line - when a new line is executed
+return - when the function returns a value
+exception - when an exception is raised
+"""
+
+def sub(x, y):
+    print(x - y)
+
+def add(x, y):
+    print(sub(x, y))
+    return x + y
+
+print(add(5, 4))
